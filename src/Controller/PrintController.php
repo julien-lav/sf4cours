a8ae4cf1134cd5b8a84d6dc6d41d93e405e30466
julien-lav <julien.laville@hotmail.fr>
2018-07-24 14:11:02 +0200

    first co'

---
+++ src/Controller/PrintController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Form\LoginType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class PrintController extends Controller
+{
+    /**
+     * @Route("/print", name="print")
+     */
+    public function index()
+    {
+
+
+
+    	$user = new User();
+    	$user->setFirstname("Paladin");
+    	$user->setBirthday(new \Datetime());
+
+    	$user2 = new User();
+    	$user2->setFirstname("Magicien");
+    	$user2->setBirthday(new \Datetime());
+
+    	$tabs = array($user, $user2);
+
+    	$em = $this->getDoctrine()->getManager();
+
+    	$users = $em->getRepository(User::class)->findAll();
+
+        return $this->render('print/index.html.twig', [
+            'controller_name' => 'PrintController',
+            'tabs' => $tabs,
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/form", name="form")
+     */
+    public function form(Request $request, UserRepository $userRepository)
+    {
+    	$user = new User();
+    	$form = $this->createForm(LoginType::class, $user);
+    	$form->handleRequest($request);
+
+    	if($form->isSubmitted() && $form->isValid()) {
+    		$em = $this->getDoctrine()->getManager();
+    		$em->persist($user);
+    		$em->flush();
+    	}
+
+    	$users = $userRepository->findAll();
+
+    	return $this->render('user/index.html.twig', array(
+    		'form' => $form->createView(),
+    		'users'=> $users,
+    	));
+
+
+
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @Assert\NotBlank()
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @Assert\Email()
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @Assert\DateTime()
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $birthday;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getBirthday(): ?\DateTimeInterface
+    {
+        return $this->birthday;
+    }
+
+    public function setBirthday(?\DateTimeInterface $birthday): self
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+}
